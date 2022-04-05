49516aee51aa39081885adf5e5fe5be4708557f0
a-s <ayn-calla@outlook.com>
2022-04-05 05:04:43 +0000

    user login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,6 +71,3 @@ class RegisterController extends Controller
         ]);
     }
 }
-// ユーザ登録
-Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
-Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
